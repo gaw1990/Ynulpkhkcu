bd671519ef4473c92105157a20ba4a6953febdd2
gaw1990 <georgewambold@gmail.com>
2016-05-25 22:32:31 -0500

    Second commit

--- small_projects/enhance_return.sh
+++
@@ -1,2 +0,0 @@
-#Do what tab would do, but by pressing return on a non-existing file
-#eg. cd + "ga" + return = "ga" + tab + return
--- small_projects/karin.rb
+++
@@ -1,9 +0,0 @@
-puts "What\'s your name?"
-name = gets.chomp
-
-if name == "karin"
-	puts "Wow. That's the most beautiufl name! =)"
-	else 
-	puts "Uhh.. That\'s a cool name.. I guess... "
-
-end
